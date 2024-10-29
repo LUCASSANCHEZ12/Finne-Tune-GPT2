ddffb116766669e34ad5e3743b8a8419ae51d6b8
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2024-10-29 18:14:02 -0400

    Added object properties to the rdf and added DB connection file (forgot)

--- back-end/rdf_generator.py
+++ back-end/rdf_generator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from rdflib import Graph, Literal, Namespace, URIRef
-from rdflib.namespace import RDF, RDFS, XSD
+from rdflib.namespace import RDF, RDFS, XSD, OWL
 import sqlalchemy
 from sqlalchemy import MetaData, inspect
 import json
@@ -25,8 +25,9 @@ def generate_rdf(output_path="output.rdf"):
 
     # Define namespaces
     KG_NS = Namespace("http://example.org/KG/")
-    knowledge_graph.bind("ley", KG_NS)
+    knowledge_graph.bind("knowledge_graph", KG_NS)
     knowledge_graph.bind("rdf", RDF)
+    knowledge_graph.bind("owl", OWL)
     knowledge_graph.bind("rdfs", RDFS)
 
     # Helper function to generate labels for resources
@@ -42,21 +43,31 @@ def generate_rdf(output_path="output.rdf"):
 
     # Add classes and properties to the knowledge graph based on the schema
     for table_name, table_info in schema_dict.items():
-        knowledge_graph.add((KG_NS[table_name], RDF.type, RDFS.Class))
+        knowledge_graph.add((KG_NS[table_name], RDF.type, OWL.Class))
         knowledge_graph.add((KG_NS[table_name], RDFS.label, Literal(table_name, datatype=XSD.string)))
 
         for column in table_info['columns']:
             column_name = column['COLUMN_NAME']
             data_type = column['DATA_TYPE']
             property_uri = KG_NS[column_name]
-            knowledge_graph.add((property_uri, RDF.type, RDF.Property))
+            knowledge_graph.add((property_uri, RDF.type, OWL.DatatypeProperty))
             knowledge_graph.add((property_uri, RDFS.label, Literal(column_name, datatype=XSD.string)))
             knowledge_graph.add((property_uri, RDFS.range, Literal(data_type, datatype=XSD.string)))
 
     # Add instances to the knowledge graph based on the data in the database
     for table in metadata.tables.keys():
+        print(f"Processing table: {table}")
         df = pd.read_sql_table(table, engine)
         foreign_keys = inspector.get_foreign_keys(table)
+        print(foreign_keys)
+
+        # Add properties for foreign keys
+        for fk in foreign_keys:
+            for constrain in fk['constrained_columns']:
+                ref_table = fk['referred_table']
+                knowledge_graph.add((KG_NS[f"has_{constrain}"], RDF.type, OWL.ObjectProperty))
+                knowledge_graph.add((KG_NS[f"has_{constrain}"], RDFS.domain, KG_NS[table]))
+                knowledge_graph.add((KG_NS[f"has_{constrain}"], RDFS.range, KG_NS[ref_table]))
 
         for _, row in df.iterrows():
             subject = URIRef(KG_NS[f"{table}/{row.iloc[0]}"])
@@ -65,20 +76,6 @@ def generate_rdf(output_path="output.rdf"):
             label = generate_label(row)
             knowledge_graph.add((subject, RDFS.label, Literal(label, datatype=XSD.string)))
 
-            for column in df.columns:
-                value = row[column]
-                if column in [fk['constrained_columns'][0] for fk in foreign_keys]:
-                    ref_table = [fk['referred_table'] for fk in foreign_keys if fk['constrained_columns'][0] == column][0]
-                    ref_subject = URIRef(KG_NS[f"{ref_table}/{value}"])
-                    knowledge_graph.add((subject, KG_NS[f"has_{ref_table}"], ref_subject))
-                else:
-                    if isinstance(value, str):
-                        knowledge_graph.add((subject, KG_NS[column], Literal(value, datatype=XSD.string)))
-                    elif isinstance(value, (int, float)):
-                        knowledge_graph.add((subject, KG_NS[column], Literal(value, datatype=XSD.double)))
-                    else:
-                        knowledge_graph.add((subject, KG_NS[column], Literal(str(value), datatype=XSD.string)))
-
-    # Serialize the knowledge graph to a Turtle file
-    knowledge_graph.serialize(destination=output_path, format="xml")
+    # Serialize the knowledge graph to a XML file
+    knowledge_graph.serialize(destination=output_path, format="pretty-xml")
     return output_path
