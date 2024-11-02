7b8374e3c39e6e8dedb2d0050e72817c28aa4501
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2024-11-01 21:52:39 -0400

    Created function consult_knowledgegraph and moved graphdb_sparql.py

--- back-end/model_for_huggingFace.py
+++ back-end/model_for_huggingFace.py
@@ -5,182 +5,225 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+def consult_knowledge_graph(question):
+    graphdb_url = os.getenv("GRAPHDB_URL")
+    token_hf = os.getenv("TOKEN_HF")
 
-graphdb_url = os.getenv("GRAPHDB_URL")
-token_hf = os.getenv("TOKEN_HF")
+    sparql = SPARQLWrapper(graphdb_url)
+    client = InferenceClient(api_key=token_hf)
 
-sparql = SPARQLWrapper(graphdb_url)
-client = InferenceClient(api_key=token_hf)
-
-rdf_schema = """
-  
-  Propiedades de objeto:
-  
-    <owl:ObjectProperty rdf:about="http://www.semanticweb.org/KG#fk_Num_ley">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
-        <rdfs:range rdf:resource="http://www.semanticweb.org/KG#ley"/>
-    </owl:ObjectProperty>
-  
-  Propiedades de datos:
-  
-    <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_Ley_ley">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#ley"/>
-        <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
-    </owl:DatatypeProperty>
+    rdf_schema = """
     
-    <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_Articulo_articulo">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
-        <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
-    </owl:DatatypeProperty>
-  
-    <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Descripcion_ley">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#ley"/>
-        <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#string"/>
-    </owl:DatatypeProperty>
-  
-    <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_ley_articulo">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
-        <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
-    </owl:DatatypeProperty>
+    Propiedades de objeto:
     
-    <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Descripcion_articulo">
-        <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
-        <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#string"/>
-    </owl:DatatypeProperty>
-  
-  Clases:
-  
-  <owl:Class rdf:about="http://www.semanticweb.org/KG#ley"/>
-  
-  <owl:Class rdf:about="http://www.semanticweb.org/KG#articulo"/>
-"""
-
-example = """
-PREFIX kg: <http://www.semanticweb.org/KG#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-SELECT ?Descripcion_ley
-WHERE {
-    ?ley kg:Num_Ley_ley 26 .
-    ?ley kg:Descripcion_ley ?Descripcion_ley .
-}
-"""
-
-question = "Describe la ley 26 con todos sus articulos"
-
-prompt = f"""
-Eres un experto en SPARQL y RDF. Tu tarea es generar consultas SPARQL precisas y correctas basadas en el esquema RDF proporcionado. A continuación se muestra el esquema RDF:
-
-{rdf_schema}
-
-Utiliza los siguientes prefijos en la consulta:
-
-PREFIX kg: <http://www.semanticweb.org/KG#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-Es **crucial** que utilices las clases y propiedades **exactamente** como aparecen en el esquema RDF, respetando mayúsculas y minúsculas.
-
-Genera **únicamente** la consulta SPARQL correcta que responda a la siguiente pregunta, sin añadir explicaciones ni comentarios adicionales:
-
-"{question}"
-
-La consulta debe:
-
-- Ser lo mas simple posible sin funciones innecesarias y parametros que no se usan.
-- Utilizar las clases y propiedades de datos y objetos tal como están definidas en el esquema RDF.
-- Respetar mayúsculas y minúsculas.
-- Ser sintácticamente correcta y válida según el esquema RDF proporcionado.
-
-Proporciona únicamente la consulta SPARQL encerrada entre tres acentos invertidos así:
-
-\`\`\`sparql
-[Tu consulta SPARQL aquí]
-\`\`\`
-
-Antes de proporcionar la consulta final, verifica que la misma es correcta y cumple con todos los requisitos.
-
-**Ejemplo de una consulta SPARQL válida:**
-
-Si la pregunta fuera "¿Cuál es la descripción de la ley con número 26?", la consulta SPARQL sería:
-
-\`\`\`sparql
-{example}
-\`\`\`
-
-Ahora, genera la consulta SPARQL que responde a la pregunta proporcionada.
-"""
-
-messages = [
-	{ "role": "user", "content": prompt }
-]
-
-stream = client.chat.completions.create(
-    model="meta-llama/Llama-3.2-3B-Instruct", 
-	messages=messages, 
-	max_tokens=500,
-	stream=False
-)
-
-responce = stream.choices[0].message.content
-#print(f"Model Responce:\n{responce}")
-
-cleaned_response = responce.replace('\\`', '`')
-
-# Opcional: Imprimir la respuesta limpiada
-#print("Respuesta después de limpiar los backticks:")
-#print(cleaned_response)
-
-pattern = r"```(?:sparql)?\n(.*?)\n```"
-match = re.search(pattern, cleaned_response, re.DOTALL)
-
-if match:
-    sparql_query = match.group(1)
-    print("\nConsulta SPARQL extraída:")
-    print(sparql_query)
-else:
-    print("No se encontró una consulta SPARQL en la respuesta.")
+        <owl:ObjectProperty rdf:about="http://www.semanticweb.org/KG#fk_Num_ley">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
+            <rdfs:range rdf:resource="http://www.semanticweb.org/KG#ley"/>
+        </owl:ObjectProperty>
     
+    Propiedades de datos:
     
-sparql.setQuery(sparql_query)
-sparql.setReturnFormat(JSON)
+        <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_Ley_ley">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#ley"/>
+            <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
+        </owl:DatatypeProperty>
+        
+        <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_Articulo_articulo">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
+            <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
+        </owl:DatatypeProperty>
+    
+        <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Descripcion_ley">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#ley"/>
+            <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#string"/>
+        </owl:DatatypeProperty>
+    
+        <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Num_ley_articulo">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
+            <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#int"/>
+        </owl:DatatypeProperty>
+        
+        <owl:DatatypeProperty rdf:about="http://www.semanticweb.org/KG#Descripcion_articulo">
+            <rdfs:domain rdf:resource="http://www.semanticweb.org/KG#articulo"/>
+            <rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#string"/>
+        </owl:DatatypeProperty>
+    
+    Clases:
+    
+    <owl:Class rdf:about="http://www.semanticweb.org/KG#ley"/>
+    
+    <owl:Class rdf:about="http://www.semanticweb.org/KG#articulo"/>
+    """
 
-results = sparql.query().convert()
+    example_pregunta_ley = """
+    PREFIX kg: <http://www.semanticweb.org/KG#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-prompt = f"""
-Eres un experto en lenguaje natural y tu tarea es interpretar los resultados de consultas SPARQL y proporcionar respuestas verbalizadas en español.
+    SELECT ?Descripcion_ley
+    WHERE {
+        ?ley kg:Num_Ley_ley 26 .
+        ?ley kg:Descripcion_ley ?Descripcion_ley .
+    }
+    """
 
-Dada la siguiente respuesta de GraphDB:
+    example_pregunta_articulo = """
+    PREFIX kg: <http://www.semanticweb.org/KG#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-{results}
+    SELECT ?Descripcion_articulo ?Num_Ley_ley
+    WHERE {
+        ?articulo a kg:articulo ;
+            kg:Num_Articulo_articulo 5135 ;
+            kg:Descripcion_articulo ?Descripcion_articulo ;
+            kg:fk_Num_ley ?ley .
+        ?ley kg:Num_Ley_ley ?Num_Ley_ley .
+    }
+    """
 
-Y considerando la consulta SPARQL utilizada:
+    example_pregunta_ley_2= """
+    PREFIX kg: <http://www.semanticweb.org/KG#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-{sparql_query}
+    SELECT ?num_ley ?Descripcion_ley ?Descripcion_articulo ?num_articulo
+    WHERE {
+        ?articulo a kg:articulo ;
+            kg:Num_Articulo_articulo ?num_articulo ;
+            kg:Descripcion_articulo ?Descripcion_articulo ;
+            kg:fk_Num_ley ?ley .
+        ?ley kg:Num_Ley_ley ?num_ley ;
+                kg:Descripcion_ley ?Descripcion_ley .
+        FILTER(?num_ley = 26)
+    }
+    """
+    prompt = f"""
+    Eres un experto en SPARQL y RDF. Tu tarea es generar consultas SPARQL precisas y correctas basadas en el esquema RDF proporcionado. 
 
-Genera una respuesta en lenguaje natural que comunique claramente la información obtenida, respondiendo a la pregunta original:
+    A continuación tienes el esquema RDF que define los elementos `Ley` y `Artículo`, y sus propiedades y relaciones, con el único vínculo a través de la propiedad `fk_Num_ley`.
 
-"{question}"
+    **Esquema RDF**:
 
-La respuesta debe:
+    {rdf_schema}
 
-- Ser clara y concisa.
-- Incluir los datos relevantes obtenidos de la consulta.
-- Estar redactada en español correcto.
-- No incluir información técnica ni detalles de la consulta.
+    **Consulta SPARQL válida para preguntas sobre leyes o artículos**:
 
-No incluyas explicaciones adicionales ni comentarios técnicos; proporciona **únicamente** la respuesta verbalizada al usuario.
-"""
+    Usa estos prefijos en cada consulta:
+    PREFIX kg: <http://www.semanticweb.org/KG#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-messages = [
-	{ "role": "user", "content": prompt }
-]
+    ### Ejemplos de Preguntas y Respuestas SPARQL
 
-stream = client.chat.completions.create(
-    model="meta-llama/Llama-3.2-3B-Instruct", 
-	messages=messages, 
-	max_tokens=500,
-	stream=False
-)
+    - **Pregunta**: "¿Qué establece la ley número 26?"
+    **Consulta SPARQL**:
+    \`\`\`sparql
+    {example_pregunta_ley}
+    \`\`\`
 
-responce = stream.choices[0].message.content
-print("Model Responce: ",responce)
+    - **Pregunta**: "¿Qué dice el artículo número 5135 de la ley 26?"
+    **Consulta SPARQL**:
+    \`\`\`sparql
+    {example_pregunta_articulo}
+    \`\`\`
+
+    - **Pregunta**: "Explica la ley 26 con todos sus artículos"
+    **Consulta SPARQL**:
+    \`\`\`sparql
+    {example_pregunta_ley_2}
+    \`\`\`
+
+    **Instrucciones para la consulta**:
+
+    Genera solo la consulta SPARQL correcta en respuesta a la pregunta del usuario usando las clases y propiedades **exactamente** como en el esquema RDF, con mayúsculas y minúsculas correspondientes.
+
+    **Antes de enviar la consulta, asegúrate de que sea válida y devuelva los resultados esperados. Valida y comprueba, si la consulta es incorrecta, genera una nueva consulta que sea correcta.**
+
+    Pregunta del usuario: "{question}"
+
+    Entrega la consulta en el formato:
+
+    \`\`\`sparql
+    [TU CONSULTA SPARQL]
+    \`\`\`
+    """
+
+
+
+    messages = [
+        { "role": "user", "content": prompt }
+    ]
+
+    stream = client.chat.completions.create(
+        model="meta-llama/Llama-3.2-3B-Instruct", 
+        messages=messages, 
+        max_tokens=500,
+        stream=False
+    )
+
+    responce = stream.choices[0].message.content
+    #print(f"Model Responce:\n{responce}")
+
+    cleaned_response = responce.replace('\\`', '`')
+
+    # Opcional: Imprimir la respuesta limpiada
+    #print("Respuesta después de limpiar los backticks:")
+    #print(cleaned_response)
+
+    pattern = r"```(?:sparql)?\n(.*?)\n```"
+    match = re.search(pattern, cleaned_response, re.DOTALL)
+
+    if match:
+        sparql_query = match.group(1)
+        print("\nConsulta SPARQL extraída:")
+        print(sparql_query)
+    else:
+        print("No se encontró una consulta SPARQL en la respuesta.")
+        
+        
+    sparql.setQuery(sparql_query)
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
+
+    prompt = f"""
+    Eres un experto en lenguaje natural y tu tarea es interpretar los resultados de consultas SPARQL y proporcionar respuestas verbalizadas en español.
+
+    Dada la siguiente respuesta de GraphDB:
+
+    {results}
+
+    Y considerando la consulta SPARQL utilizada:
+
+    {sparql_query}
+
+    Genera una respuesta en lenguaje natural que comunique claramente la información obtenida, respondiendo a la pregunta original:
+
+    "{question}"
+
+    La respuesta debe:
+
+    - Ser clara y concisa.
+    - Incluir los datos relevantes obtenidos de la consulta.
+    - Estar redactada en español correcto.
+    - No incluir información técnica ni detalles de la consulta.
+
+    No incluyas explicaciones adicionales ni comentarios técnicos; proporciona **únicamente** la respuesta verbalizada al usuario.
+    """
+
+    messages = [
+        { "role": "user", "content": prompt }
+    ]
+
+    stream = client.chat.completions.create(
+        model="meta-llama/Llama-3.2-3B-Instruct", 
+        messages=messages, 
+        max_tokens=500,
+        stream=False
+    )
+
+    response = stream.choices[0].message.content
+    print("Model Response: ",response)
+
+    return response
+
+
+question = "¿Cuáles son los artículos incluidos en la ley número 40 y qué establecen?"
+consult_knowledge_graph(question)
