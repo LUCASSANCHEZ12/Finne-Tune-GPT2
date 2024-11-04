031a6f28d292434a68084c169842d3e78f54b062
Nicolas Vargas <nicovarojas@gamil.com>
2024-11-04 00:16:33 -0400

    Added call to backend

--- front-end/src/ChatbotAPI.jsx
+++ front-end/src/ChatbotAPI.jsx
@@ -2,6 +2,14 @@ const API = {
   GetChatbotResponse: async message => {
     return new Promise(function(resolve, reject) {
       console.log("Sending message to chatbot model: ", message);
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "prompt": message })
+    };
+    fetch('http://localhost:800/prompt', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
       setTimeout(function() {
         if (message === "hi") resolve("Welcome to chatbot!");
         else resolve("echo : " + message);
